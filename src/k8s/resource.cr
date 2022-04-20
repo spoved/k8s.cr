87a24446b15a4542dbb4b5cbb15ef8648cdcbca6
Holden Omans <holden.omans@gmail.com>
2022-04-20 16:27:07 -0400

    removed apiVersion and kind check in K8S::Kubernetes::Resource

--- src/k8s/resource.cr
+++ src/k8s/resource.cr
@@ -10,18 +10,6 @@ abstract struct K8S::Kubernetes::Resource < K8S::Kubernetes::Object
   define_prop api_version : String, key: "apiVersion", nilable: false, read_only: true, description: %<APIVersion defines the versioned schema of this representation of an object. Servers should convert recognized schemas to the latest internal value, and may reject unrecognized values. More info: [[[https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#resources](https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#resources)](https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#resources](https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#resources))](https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#resources](https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#resources)](https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#resources](https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#resources)))>
   define_prop kind : String, nilable: false, read_only: true, description: %<Kind is a string value representing the REST resource this object represents. Servers may infer this from the endpoint the client submits requests to. Cannot be updated. In CamelCase. More info: [[[https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#types-kinds](https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#types-kinds)](https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#types-kinds](https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#types-kinds))](https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#types-kinds](https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#types-kinds)](https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#types-kinds](https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#types-kinds)))>
 
-  def initialize(hash : Enumerable | Iterable | NamedTuple | Nil = nil)
-    super
-    raise K8S::Kubernetes::Resource::Error.new "apiVersion must be defined" if @__object__["apiVersion"]?.nil?
-    raise K8S::Kubernetes::Resource::Error.new "kind must be defined" if @__object__["kind"]?.nil?
-  end
-
-  def initialize(obj : K8S::Internals::GenericObject)
-    super
-    raise K8S::Kubernetes::Resource::Error.new "apiVersion must be defined" if @__object__["apiVersion"]?.nil?
-    raise K8S::Kubernetes::Resource::Error.new "kind must be defined" if @__object__["kind"]?.nil?
-  end
-
   def self.new(pull : ::JSON::PullParser)
     obj = K8S::Internals::GenericObject.new(pull)
     K8S::Util.new_resource(obj)
