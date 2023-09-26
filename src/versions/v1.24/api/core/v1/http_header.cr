84e72bba4dba194fc866610ac5f058a4536b9579
Holden Omans <holden.omans@gmail.com>
2023-09-26 09:45:11 -0400

    updates to 1.24-1.27 comments

--- src/versions/v1.24/api/core/v1/http_header.cr
+++ src/versions/v1.24/api/core/v1/http_header.cr
@@ -7,7 +7,7 @@ require "json"
   namespace: "::K8S::Api::Core::V1",
   properties: [
 
-    {name: "name", kind: String, key: "name", nilable: false, read_only: false, description: "The header field name"},
+    {name: "name", kind: String, key: "name", nilable: false, read_only: false, description: "The header field name. This will be canonicalized upon output, so case-variant names will be understood as the same header."},
     {name: "value", kind: String, key: "value", nilable: false, read_only: false, description: "The header field value"},
 
   ]
